f87fbee1bb470c8fe68bcbe0d268ca3ffee9e2d1
Brian Anderson <andersrb@gmail.com>
2010-06-24 23:31:10 -0400

    Add logging

--- src/main/scala/net/negatory/quickshake/quickshake.scala
+++ src/main/scala/net/negatory/quickshake/quickshake.scala
@@ -6,12 +6,15 @@ object QuickShake {
 
   def main(args: Array[String]) {
 
+    val logger = new ConsoleLogger
+    import logger._
+
     val options = new Options(args)
 
-    println ("indirs: ")
-    options.indirs foreach (println _)
-    println ("outdir: " + options.outdir)
-    println ("keepNamespace: " + options.keepNamespace)
+    info("indirs: ")
+    options.indirs foreach (info _)
+    info("outdir: " + options.outdir)
+    info("keepNamespace: " + options.keepNamespace)
   }
 
 }
@@ -21,4 +24,35 @@ class Options(args: Array[String]) {
   val indirs = args(0) split ":"
   val outdir = args(1)
   val keepNamespace = args(2)
+}
+
+object LogLevel extends Enumeration {
+  type LogLevel = Value
+  val Debug, Info, Warning, Error = Value
+}
+
+trait Logger {
+
+  import LogLevel._
+
+  val minLogLevel: LogLevel
+
+  def debug(msg: String): Unit = log(Debug, msg)
+  def info(msg: String): Unit = log(Info, msg)
+  def warning(msg: String): Unit = log(Warning, msg)
+  def error(msg: String): Unit = log(Error, msg)
+
+  protected[this] def log(level: LogLevel, msg: String)
+}
+
+import LogLevel.LogLevel
+
+class ConsoleLogger extends Logger {
+
+  override val minLogLevel = LogLevel.Debug
+
+  protected[this] override def log(level: LogLevel, msg: String) {
+    println(level.toString + ": " + msg)
+  }
+
 }
