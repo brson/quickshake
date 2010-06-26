f0dd7d281bda9c28179820a96d62df32abc6d09c
Brian Anderson <andersrb@gmail.com>
2010-06-26 03:44:27 -0400

    Add a temporary test script

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+java -cp target/scala_2.8.0.RC6/classes/:/home/brian/.ivy2/cache/org.scala-lang/scala-library/jars/scala-library-2.8.0.RC6.jar net.negatory.quickshake.QuickShake /home/brian/Dev/stoptime/target/scala_2.8.0-local/classes/:/home/brian/Dev/scala/build/quick/classes/library/ testoutput/ net.negatory.stoptime
+
