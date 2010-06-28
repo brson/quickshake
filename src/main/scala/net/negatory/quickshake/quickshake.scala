21b72f0758e60f589782e4c578ee37a4b9de3349
Brian Anderson <andersrb@gmail.com>
2010-06-28 00:25:06 -0400

    Cleanup

--- src/main/scala/net/negatory/quickshake/quickshake.scala
+++ src/main/scala/net/negatory/quickshake/quickshake.scala
@@ -305,36 +305,50 @@ object ClassCounter {
 
 class ClassCounter extends Actor {
   import ClassCounter._
+  import actors.OutputChannel
 
   var inspected = 0
   var kept = 0
   var discarded = 0
+
+  var resultRequester: Option[OutputChannel[Any]] = None
+
+  def checkResults() = if (kept + discarded == inspected) {
+    assert(resultRequester.isDefined)
+    resultRequester.get ! Results(inspected, kept, discarded)
+    exit
+  }
 
   def act() = loop {
     react {
       case Inspected => inspected += 1
       case Kept => kept += 1
       case Discarded => discarded += 1
-      case GetResults =>
-	val resultRequester = sender
-	def checkResults() = if (kept + discarded == inspected) {
-	  resultRequester ! Results(inspected, kept, discarded)
-	  exit
-	}
+      case GetResults => {
+	resultRequester = Some(sender)
 	checkResults()
 	// If we haven't heard back from all the classes we inspected
 	// then keep waiting for the numbers to add up
-	loop { react {
-	  case Inspected =>
-	    inspected += 1
-	    checkResults()
-	  case Kept =>
-	    kept += 1
-	    checkResults()
-	  case Discarded =>
-	    discarded += 1
-	    checkResults()
-	}}
+	waitForResults()
+      }
     }
   }
+
+  private def waitForResults() = loop {
+    react {
+      case Inspected => {
+	inspected += 1
+	checkResults()
+      }
+      case Kept => {
+	kept += 1
+	checkResults()
+      }
+      case Discarded => {
+	discarded += 1
+	checkResults()
+      }
+    }
+  }
+
 }
