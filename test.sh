dd2415e9b0036962a254d6dc1998c0cb3dab929b
Brian Anderson <andersrb@gmail.com>
2010-06-29 21:29:08 -0400

    Point the tests at the correct version of Scala

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-java -cp target/scala_2.7.7/classes/:/home/brian/.ivy2/cache/org.scala-lang/scala-library/jars/scala-library-2.7.7.jar:/home/brian/.ivy2/cache/commons-io/commons-io/jars/commons-io-1.4.jar:/home/brian/.ivy2/cache/asm/asm/jars/asm-3.2.jar:/home/brian/.ivy2/cache/asm/asm-commons/jars/asm-commons-3.2.jar net.negatory.quickshake.QuickShake /home/brian/Dev/stoptime/target/scala_2.7.7-local/classes/:/home/brian/Dev/scala/build/quick/classes/library/ testoutput/ net.negatory.stoptime $1
+java -cp target/scala_2.7.7/classes/:/home/brian/.ivy2/cache/org.scala-lang/scala-library/jars/scala-library-2.7.7.jar:/home/brian/.ivy2/cache/commons-io/commons-io/jars/commons-io-1.4.jar:/home/brian/.ivy2/cache/asm/asm/jars/asm-3.2.jar:/home/brian/.ivy2/cache/asm/asm-commons/jars/asm-commons-3.2.jar net.negatory.quickshake.QuickShake /home/brian/Dev/stoptime/target/scala_2.8.0-local/classes/:/home/brian/Dev/scala/build/quick/classes/library/ testoutput/ net.negatory.stoptime $1
 
