4a846f1a00daa8e24ad982968886f286a2810b9d
Brian Anderson <andersrb@gmail.com>
2010-06-29 21:16:52 -0400

    Fix logLevel selection

--- src/main/scala/net/negatory/quickshake/Logging.scala
+++ src/main/scala/net/negatory/quickshake/Logging.scala
@@ -13,11 +13,11 @@ trait Logger {
 
   // Allows creation of a single logger, then mixing in the instance's Mixin trait
   trait LoggerMixin extends Logging {
-    override val minLogLevel = Logger.this.minLogLevel
+    override val minLevel = Logger.this.minLevel
     override def log(level: LogLevel, msg: String) = Logger.this.log(level, msg)
   }
 
-  val minLogLevel: LogLevel = Debug
+  def minLevel: LogLevel = Debug
 
   // TODO: Consider eliding this method
   def debug(msg: => String): Unit = trylog(Debug, msg)
@@ -26,16 +26,24 @@ trait Logger {
   def error(msg: => String): Unit = trylog(Error, msg)
 
   def trylog(level: LogLevel, msg: => String) {
-    if (level >= minLogLevel) log(level, msg)
+    if (level >= minLevel) log(level, msg)
   }
 
   def log(level: LogLevel, msg: String) {}
 }
 
-class ConsoleLogger(override val minLogLevel: LogLevel) extends Logging {
+class ConsoleLogger(level: LogLevel) extends Logging {
+
+  override def minLevel: LogLevel = level
 
   override def log(level: LogLevel, msg: String) {
-    println(level.toString + ": " + msg)
+    val levelStr = level match {
+      case Debug => "debug"
+      case Info  => "info"
+      case Warning => "warning"
+      case Error => "error"
+    }
+    println(levelStr + ": " + msg)
   }
 
 }
