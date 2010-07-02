3bf9eb15ab6efed22bf0d5b4d53d60dc9e4256b8
Brian Anderson <andersrb@gmail.com>
2010-07-02 17:09:04 -0400

    Remove old classpath from script

--- test.sh
+++ test.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 
 QUICKSHAKE_CP=target/scala_2.7.7/classes/
-SCALA_CP=/home/brian/.ivy2/cache/org.scala-lang/scala-library/jars/scala-library-2.7.7.jar
-#SCALA_CP=project/boot/scala-2.7.7/lib/scala-library.jar
+SCALA_CP=project/boot/scala-2.7.7/lib/scala-library.jar
 
-#LIBRARY_DIR=lib_managed/scala_2.7.7/compile
-#COMMONS_IO=$LIBRARY_DIR/commons-io-1.4.jar
-#ASM=$LIBRARY_DIR/asm-3.2.jar
-#ASM_COMMONS=$LIBRARY_DIR/asm-commons-3.2.jar
-
-LIBRARY_DIR=/home/brian/.ivy2/cache
-COMMONS_IO=$LIBRARY_DIR/commons-io/commons-io/jars/commons-io-1.4.jar
-ASM=$LIBRARY_DIR/asm/asm/jars/asm-3.2.jar
-ASM_COMMONS=$LIBRARY_DIR/asm/asm-commons/jars/asm-commons-3.2.jar
+LIBRARY_DIR=lib_managed/scala_2.7.7/compile
+COMMONS_IO=$LIBRARY_DIR/commons-io-1.4.jar
+ASM=$LIBRARY_DIR/asm-3.2.jar
+ASM_COMMONS=$LIBRARY_DIR/asm-commons-3.2.jar
 
 LIB_CP=$COMMONS_IO:$ASM:$ASM_COMMONS
 
