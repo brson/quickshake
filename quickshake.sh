5f1d265f8f623348c5823305631bca80ec84f01a
Brian Anderson <andersrb@gmail.com>
2010-07-02 17:12:34 -0400

    Seperated the test script into quickshake.sh + test.sh

---
+++ quickshake.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+QUICKSHAKE_CP=target/scala_2.7.7/classes/
+SCALA_CP=project/boot/scala-2.7.7/lib/scala-library.jar
+
+LIBRARY_DIR=lib_managed/scala_2.7.7/compile
+COMMONS_IO=$LIBRARY_DIR/commons-io-1.4.jar
+ASM=$LIBRARY_DIR/asm-3.2.jar
+ASM_COMMONS=$LIBRARY_DIR/asm-commons-3.2.jar
+
+LIB_CP=$COMMONS_IO:$ASM:$ASM_COMMONS
+
+export CLASSPATH=$QUICKSHAKE_CP:$SCALA_CP:$LIB_CP
+
+MAIN=net.negatory.quickshake.QuickShake
+
+java $MAIN $@
--- test.sh
+++ test.sh
@@ -1,21 +1,8 @@
 #!/bin/sh
 
-QUICKSHAKE_CP=target/scala_2.7.7/classes/
-SCALA_CP=project/boot/scala-2.7.7/lib/scala-library.jar
-
-LIBRARY_DIR=lib_managed/scala_2.7.7/compile
-COMMONS_IO=$LIBRARY_DIR/commons-io-1.4.jar
-ASM=$LIBRARY_DIR/asm-3.2.jar
-ASM_COMMONS=$LIBRARY_DIR/asm-commons-3.2.jar
-
-LIB_CP=$COMMONS_IO:$ASM:$ASM_COMMONS
-
-CLASSPATH=$QUICKSHAKE_CP:$SCALA_CP:$LIB_CP
-
-MAIN=net.negatory.quickshake.QuickShake
 INDIRS=/home/brian/Dev/stoptime/target/scala_2.8.0-local/classes/:/home/brian/Dev/scala/build/quick/classes/library/
 OUTDIR=testoutput/
 KEEP=net.negatory.stoptime
 
-java -cp $CLASSPATH $MAIN $INDIRS $OUTDIR $KEEP $1
+./quickshake.sh $INDIRS $OUTDIR $KEEP $1
 
