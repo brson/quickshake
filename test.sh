96ca350ad0eba975a896fff7d7257c77c8ec4978
Brian Anderson <andersrb@gmail.com>
2010-07-03 20:14:31 -0400

    Read scala-library from the jar

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-INDIRS=/home/brian/Dev/stoptime/target/scala_2.8.0-local/classes/:/home/brian/Dev/scala/build/quick/classes/library/
+INDIRS=/home/brian/Dev/stoptime/target/scala_2.8.0-local/classes/:/home/brian/Dev/scala/build/pack/lib/scala-library.jar
 OUTDIR=testoutput/
 KEEP=net.negatory.stoptime
 
