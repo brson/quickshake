9a15257ac3193596b2d98b4cc079adb955b8c77b
Brian Anderson <andersrb@gmail.com>
2010-07-13 20:28:40 -0400

    Use ASM to generate the new bytecode, but don't strip methods yet

--- src/main/scala/net/negatory/quickshake/ClassEncoder.scala
+++ src/main/scala/net/negatory/quickshake/ClassEncoder.scala
@@ -3,11 +3,21 @@ package net.negatory.quickshake
 object ClassEncoder {
 
   import collection.mutable.HashSet
+  import org.objectweb.asm._
 
   def stripClass(
     classData: Array[Byte],
     keptMethods: HashSet[String]
   ): Array[Byte] = {
-    classData
+    val reader = new ClassReader(classData)
+    // TODO: Providing reader to writer is supposed to be
+    // an optimization. Does it work here?
+    val writer = new ClassWriter(reader, 0)
+    val adapter = new ClassAdapter(writer) {
+    }
+
+    reader.accept(adapter, 0)
+
+    writer.toByteArray()
   }
 }
