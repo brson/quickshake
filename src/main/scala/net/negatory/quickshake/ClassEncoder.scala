d6ea82afe71b8371d3deede7919969af0587a6ab
Brian Anderson <andersrb@gmail.com>
2010-07-13 20:42:32 -0400

    Drop methods that aren't kept

--- src/main/scala/net/negatory/quickshake/ClassEncoder.scala
+++ src/main/scala/net/negatory/quickshake/ClassEncoder.scala
@@ -14,6 +14,23 @@ object ClassEncoder {
     // an optimization. Does it work here?
     val writer = new ClassWriter(reader, 0)
     val adapter = new ClassAdapter(writer) {
+      override def visitMethod(
+	access: Int,
+	name: String,
+	desc: String,
+	signature: String,
+	exceptions: Array[String]
+      ): MethodVisitor = {
+	if (keptMethods contains name) {
+	  super.visitMethod(
+	    access,
+	    name,
+	    desc,
+	    signature,
+	    exceptions
+	  )
+	} else null
+      }
     }
 
     reader.accept(adapter, 0)
