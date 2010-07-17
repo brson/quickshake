b9bedd299cc672c805a81be44c4d6a5fa2e7d4ed
Brian Anderson <andersrb@gmail.com>
2010-07-17 16:44:08 -0400

    Extract method keepClass to flatten the code in ClassCoordinator

--- src/main/scala/net/negatory/quickshake/ClassCoordinator.scala
+++ src/main/scala/net/negatory/quickshake/ClassCoordinator.scala
@@ -16,57 +16,12 @@ class ClassCoordinator(
     decoder ! ClassDecoder.GetProps
 
     react {
-      case ClassDecoder.Props(ClassProps(className, _, _)) =>
+      case ClassDecoder.Props(classProps @ ClassProps(className, _, _)) =>
 	debug("Decoded name of class " + className)
 	decider ! KeepClassDecider.DecideOnClass(className)
 	react {
 	  case KeepClassDecider.Kept =>
-	    debug("Keeping class " + className)
-	    decoder ! ClassDecoder.FindDependencies
-	    var methods = 0
-	    loop {
-	      react {
-		case ClassDecoder.ClassDependency(depName) =>
-		  decider ! KeepClassDecider.KeepClass(depName)
-		case ClassDecoder.Method(methodName, classDeps, methodDeps) =>
-		  methods += 1
-		  val props = MethodProps(className, methodName, classDeps, methodDeps)
-		  decider ! KeepClassDecider.DecideOnMethod(props)
-		case ClassDecoder.End =>
-		  // Get the list of methods to keep
-		  import collection.mutable.HashSet
-		  var methodsDecided = 0
-		  val methodsKept = new HashSet[String]
-		  loopWhile(methodsDecided < methods) {
-		    react {
-		      val f: PartialFunction[Any, Unit] = {
-			case KeepClassDecider.KeptMethod(props) =>
-			  val MethodProps(_, methodName, classDeps, methodDeps) = props
-			  debug("Keeping method " + methodName)
-			  statsTracker ! StatsTracker.KeptMethod
-			  methodsKept += methodName
-			  classDeps foreach {
-			    decider ! KeepClassDecider.KeepClass(_)
-			  }
-			  methodDeps foreach { p =>
-			    val (cn, mn) = p
-			    decider ! KeepClassDecider.KeepMethod(cn, mn)
-			  }
-			case KeepClassDecider.DiscardedMethod(props) =>
-			  val MethodProps(_, methodName, _, _) = props
-			  debug("Discarding method " + methodName)
-			  statsTracker ! StatsTracker.DiscardedMethod
-		      }
-
-		      f andThen { _ => methodsDecided += 1 }
-		    }
-		  } andThen {
-		    statsTracker ! StatsTracker.KeptClass
-		    dataWriter ! ClassDataWriter.AddClass(className, classData, methodsKept)
-		    exit()
-		  }
-	      }
-	    }
+	    keepClass(classProps)
 	  case KeepClassDecider.Discarded => 
 	    debug("Discarding class " + className)
 	    decoder ! ClassDecoder.Discard
@@ -75,4 +30,56 @@ class ClassCoordinator(
 	}
     }
   }
+
+  private[this] def keepClass(classProps: ClassProps) {
+    val ClassProps(className, _, _) = classProps
+    debug("Keeping class " + className)
+    decoder ! ClassDecoder.FindDependencies
+    var methods = 0
+    loop {
+      react {
+	case ClassDecoder.ClassDependency(depName) =>
+	  decider ! KeepClassDecider.KeepClass(depName)
+	case ClassDecoder.Method(methodName, classDeps, methodDeps) =>
+	  methods += 1
+	  val props = MethodProps(className, methodName, classDeps, methodDeps)
+	  decider ! KeepClassDecider.DecideOnMethod(props)
+	case ClassDecoder.End =>
+	  // Get the list of methods to keep
+	  import collection.mutable.HashSet
+	  var methodsDecided = 0
+	  val methodsKept = new HashSet[String]
+	  loopWhile(methodsDecided < methods) {
+	    react {
+	      val f: PartialFunction[Any, Unit] = {
+		case KeepClassDecider.KeptMethod(props) =>
+		  val MethodProps(_, methodName, classDeps, methodDeps) = props
+		  debug("Keeping method " + methodName)
+		  statsTracker ! StatsTracker.KeptMethod
+		  methodsKept += methodName
+		  classDeps foreach {
+		    decider ! KeepClassDecider.KeepClass(_)
+		  }
+		  methodDeps foreach {
+		    p =>
+		      val (cn, mn) = p
+		      decider ! KeepClassDecider.KeepMethod(cn, mn)
+		  }
+		case KeepClassDecider.DiscardedMethod(props) =>
+		  val MethodProps(_, methodName, _, _) = props
+		  debug("Discarding method " + methodName)
+		  statsTracker ! StatsTracker.DiscardedMethod
+	      }
+
+	      f andThen { _ => methodsDecided += 1 }
+	    }
+	  } andThen {
+	    statsTracker ! StatsTracker.KeptClass
+	    dataWriter ! ClassDataWriter.AddClass(className, classData, methodsKept)
+	    exit()
+	}
+      }
+    }
+  }
+
 }
